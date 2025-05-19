5b1bc9fa2bb3299b1fbfc17e6770d0f2aff0838e
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-05-19 22:51:49 +0530

    Adding CurrentUser Route, signin Route and signout route in userservice

--- userservice/src/routes/Currentuser.ts
+++ userservice/src/routes/Currentuser.ts
@@ -1,9 +1,13 @@
 import express  from "express";
+import jwt from 'jsonwebtoken';
+import { currentUser } from "../middlewares/currentUser";
+import { requireAuth } from "../middlewares/require-auth";
 
 const Router = express.Router();
 
-Router.get('api/users/currentuser', (req, res)=>{
-
+Router.get('api/users/currentuser', currentUser, requireAuth,(req, res)=>{
+  res.send({currentUser: req.currentUser});
+  
 })
 
 export {Router as CurrentUserRouter};
--- userservice/src/routes/Signout.ts
+++ userservice/src/routes/Signout.ts
@@ -3,7 +3,9 @@ import express from 'express';
 const Router = express.Router();
 
 Router.post('api/users/signout',(req, res)=>{
+  req.session = null;
 
+  res.send({});
 })
 
 export {Router as SignOutRouter};
