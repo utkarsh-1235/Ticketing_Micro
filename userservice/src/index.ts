1badafde9e2bebcd34e2c5a47b66a4b0c0439e78
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-05-20 22:38:39 +0530

    Modifying Index.ts in userservice

--- userservice/src/index.ts
+++ userservice/src/index.ts
@@ -1,41 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { trusted } from 'mongoose';
-import cookieSession from 'cookie-session';
-import cookieParser from 'cookie-parser';   
-import morgan from 'morgan';
+import {app} from './server'
 import mongoose from 'mongoose';
-import { SignUpRouter } from './routes/Signup';
-import { SignOutRouter } from './routes/Signout';
-import { CurrentUserRouter } from './routes/Currentuser';
-import { SignInRouter } from './routes/Signin';
-import { NotFound } from './error/NotFound.error';
-import { errorHandler } from './middlewares/error-handler';
-
-const app = express(); 
+import dotenv from 'dotenv';
 dotenv.config();
 const Port = process.env.PORT;
-app.set('trust proxy', true); // trust first proxy
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(morgan('dev')); 
-app.use(cookieParser());
-
-app.use(cookieSession({
-    signed: false,
-    secure: trusted
-}));
-
-app.use(morgan('dev'));
-app.get('*', ()=>{
-    throw new NotFound();
-})
-
-app.use(errorHandler);
 
 const start = async()=>{
     try{
-        if(!process.env.JET_KEY){
+        if(!process.env.JWT_KEY){
             throw new Error("JWT_KEY must be defined");
         }
     await mongoose.connect(process.env.MONGO_URI)
@@ -47,10 +18,6 @@ const start = async()=>{
         console.error(err);
     }
 }
-app.use(SignUpRouter);
-app.use(SignOutRouter);
-app.use(CurrentUserRouter);
-app.use(SignInRouter);
 
 app.listen(Port, ()=>{
     console.log(`Server is running on port ${Port}`)
@@ -58,4 +25,3 @@ app.listen(Port, ()=>{
 
 start();
 
-module.exports = app;
