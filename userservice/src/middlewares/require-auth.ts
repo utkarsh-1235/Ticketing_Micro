af8041f4d319790ec67c70c6936af87debeaff51
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-05-20 22:46:13 +0530

    Modifying index.ts in userservice

--- userservice/src/middlewares/require-auth.ts
+++
@@ -1,9 +0,0 @@
-import { NotAuthorizedError } from "../error/NotAuthorizedError";
-import {Request, Response, NextFunction}l from 'express';
-
-export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
-    if(!req.currentUser){
-         throw new NotAuthorizedError();
-    }
-    next();
-}
--- userservice/src/server.ts
+++
@@ -1,5 +0,0 @@
-import app from './index';
-const Port = 4000;
-app.listen(Port, ()=>{
-    console.log(`Server is running on port ${Port}`)
-})
